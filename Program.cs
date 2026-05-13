d9f628ba371aec36a4ee983e2c1b7ef6b4f71b46
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-05-13 07:24:06 +0545

    event handaling

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//event handling of message queue
+
+using System;
+public class MessageQueue
+{
+    public event Action ProcessCompleted;
+
+protected void  OnProcessCompleted()
+{
+    ProcessCompleted?.Invoke();
+
+}
+}
+class Program
+{
+  public static void Message()
+    {
+        Console.WriteLine("Message Processed");
+    }
+         static void Main(string[] args)
+        {
+           Process p = new Process();
+              p.ProcessCompleted += Message;
+    p.StartProcess();
+  }
+}
