73c2a16945f11b2243c9336041df687d196b003a
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-05-18 06:39:12 +0545

    making traingle

---
+++ triangle.cs
@@ -0,0 +1,23 @@
+
+using System;
+using System.Globalization;
+
+class Program
+{
+    
+    
+    static void Main(string[] args)
+    {
+     
+ Console.WriteLine("Triangle pattern:");
+int rows = 5;
+for (int i = 1; i <= rows; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+}
+    }
